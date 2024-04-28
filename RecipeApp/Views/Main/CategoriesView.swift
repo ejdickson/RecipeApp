c41bd46385c9029459ed91728c3b41298f6543e3
Emma Brennan <ejdickson93@gmail.com>
2024-04-27 20:13:52 -0400

    Initialized main view files (only sample code in views).

---
+++ RecipeApp/Views/Main/CategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesView.swift
+//  RecipeApp
+//
+//  Created by Emma Brennan on 4/27/24.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesView()
+}
