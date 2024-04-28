1a0e74b84e91f28244a21178abf743a62a58f950
Emma Brennan <ejdickson93@gmail.com>
2024-04-27 20:36:13 -0400

    Menu bar functionality added, placeholder code on views.

---
+++ RecipeApp/Views/Main/GroceryListView.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryListView.swift
+//  RecipeApp
+//
+//  Created by Emma Brennan on 4/27/24.
+//
+
+import SwiftUI
+
+struct GroceryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroceryListView()
+}
